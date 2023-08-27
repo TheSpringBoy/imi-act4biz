5725b4116b3809573fa4a110456be387741bcdd6
TheSpringBoy <75606783+TheSpringBoy@users.noreply.github.com>
2023-08-27 18:59:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const showFormButton = document.getElementById("showForm");
-  const iframeForm = document.getElementById("iframeForm");
-  const iframeDashboard = document.getElementById("iframeDashboard");
-  const menuLinks = document.querySelectorAll(".menu a");
+    const showFormButton = document.getElementById("showForm");
+    const iframeForm = document.getElementById("iframeForm");
+    const iframeDashboard = document.getElementById("iframeDashboard");
+    const menuLinks = document.querySelectorAll(".menu a");
 
-  // Handler for the "Request New Article" button
-  showFormButton.addEventListener("click", function () {
-    iframeForm.src = "https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc";
-    displayIframe(iframeForm);
-  });
-
-  // Handler for the navigation menu
-  menuLinks.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const target = this.getAttribute("href");
-      
-      if (target === "#dashboard") {
-        iframeDashboard.src = "https://actnews.monday.com/boards/1253459064/views/5983866";
-        displayIframe(iframeDashboard);
-      } else if (target === "#request-form") {
+    showFormButton.addEventListener("click", function () {
         iframeForm.src = "https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc";
-        displayIframe(iframeForm);
-      } else {
-        hideAllIframes();
-      }
-      scrollToSection(target);
+        iframeForm.style.display = "block";
+        iframeDashboard.style.display = "none"; // Hide dashboard iframe
     });
-  });
 
-  function displayIframe(iframeToShow) {
-    // Hide both iframes first
-    iframeForm.style.display = "none";
-    iframeDashboard.style.display = "none";
-
-    // Display the selected iframe
-    iframeToShow.style.display = "block";
-  }
-
-  function hideAllIframes() {
-    iframeForm.style.display = "none";
-    iframeDashboard.style.display = "none";
-  }
-
-  function scrollToSection(target) {
-    const section = document.querySelector(target);
-    window.scrollTo({
-      top: section.offsetTop,
-      behavior: "smooth",
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const target = this.getAttribute("href");
+            if (target === "#dashboard") {
+                iframeDashboard.style.display = "block";
+                iframeForm.style.display = "none"; // Hide form iframe
+            } else {
+                iframeDashboard.style.display = "none"; // Hide dashboard iframe
+                iframeForm.style.display = "block";
+            }
+            scrollToSection(target);
+        });
     });
-  }
+
+    function scrollToSection(target) {
+        const section = document.querySelector(target);
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: "smooth",
+        });
+    }
 });
