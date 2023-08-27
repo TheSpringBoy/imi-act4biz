3a9cac21e55bee8ff200371e66e0d8d9754587b6
TheSpringBoy <75606783+TheSpringBoy@users.noreply.github.com>
2023-08-27 19:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const iframeSections = document.querySelectorAll('.iframe-section');
     const linksAndButtons = document.querySelectorAll('[data-target]');
     const landingSection = document.querySelector('.landing');
+    const defaultViewSection = document.getElementById('default-view');
     
     linksAndButtons.forEach(element => {
         element.addEventListener('click', function(e) {
-            e.preventDefault();
-            
-            // Hide all iframe sections
-            iframeSections.forEach(section => section.style.display = 'none');
-            
-            // Hide the landing section
-            landingSection.style.display = 'none';
-            
-            // Show target iframe section
             const targetId = this.getAttribute('data-target');
-            const target = document.getElementById(targetId);
-            target.style.display = 'block';
             
             if (targetId === 'request-form') {
+                e.preventDefault();
+                
+                // Hide all iframe sections
+                iframeSections.forEach(section => section.style.display = 'none');
+                
+                // Hide the landing section
+                landingSection.style.display = 'none';
+                
+                // Hide the default view section
+                defaultViewSection.style.display = 'none';
+                
+                // Show target iframe section
+                const target = document.getElementById(targetId);
+                target.style.display = 'block';
                 target.querySelector('iframe').src = 'https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc';
-            } else if (targetId === 'dashboard') {
-                target.querySelector('iframe').src = 'https://actnews.monday.com/boards/1253459064/views/5983866';
             }
         });
     });
