32d96a723b29795df086f0d96d200c524442f7fc
TheSpringBoy <75606783+TheSpringBoy@users.noreply.github.com>
2023-08-27 19:04:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const showFormButton = document.getElementById("showForm");
     const iframeForm = document.getElementById("iframeForm");
-    const iframeDashboard = document.getElementById("iframeDashboard");
-    const menuLinks = document.querySelectorAll(".menu a");
 
     showFormButton.addEventListener("click", function () {
         iframeForm.src = "https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc";
-        iframeForm.style.display = "block";
-        iframeDashboard.style.display = "none"; // Hide dashboard iframe
+        iframeForm.style.display = "block"; // This should make the iframe visible
     });
-
-    menuLinks.forEach((link) => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            const target = this.getAttribute("href");
-            if (target === "#dashboard") {
-                iframeDashboard.style.display = "block";
-                iframeForm.style.display = "none"; // Hide form iframe
-            } else {
-                iframeDashboard.style.display = "none"; // Hide dashboard iframe
-                iframeForm.style.display = "block";
-            }
-            scrollToSection(target);
-        });
-    });
-
-    function scrollToSection(target) {
-        const section = document.querySelector(target);
-        window.scrollTo({
-            top: section.offsetTop,
-            behavior: "smooth",
-        });
-    }
 });
