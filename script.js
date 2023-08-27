fc30ca82da9a26f9676aa664462cbb464651c3d6
TheSpringBoy <75606783+TheSpringBoy@users.noreply.github.com>
2023-08-27 18:54:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const iframeDashboard = document.getElementById("iframeDashboard");
   const menuLinks = document.querySelectorAll(".menu a");
 
+  // Handler for the "Request New Article" button
   showFormButton.addEventListener("click", function () {
-    iframeForm.style.display = "block";
     iframeForm.src = "https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc";
-    iframeDashboard.style.display = "none"; // Hide dashboard iframe
+    displayIframe(iframeForm);
   });
 
+  // Handler for the navigation menu
   menuLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
       const target = this.getAttribute("href");
+      
       if (target === "#dashboard") {
-        iframeDashboard.style.display = "block";
         iframeDashboard.src = "https://actnews.monday.com/boards/1253459064/views/5983866";
-        iframeForm.style.display = "none"; // Hide form iframe
+        displayIframe(iframeDashboard);
+      } else if (target === "#request-form") {
+        iframeForm.src = "https://www.superform.spot-nik.com/form/64e631c4b0bcbd51295181bc";
+        displayIframe(iframeForm);
       } else {
-        iframeForm.style.display = "none"; // Hide form iframe
-        iframeDashboard.style.display = "none"; // Hide dashboard iframe
+        hideAllIframes();
       }
       scrollToSection(target);
     });
   });
+
+  function displayIframe(iframeToShow) {
+    // Hide both iframes first
+    iframeForm.style.display = "none";
+    iframeDashboard.style.display = "none";
+
+    // Display the selected iframe
+    iframeToShow.style.display = "block";
+  }
+
+  function hideAllIframes() {
+    iframeForm.style.display = "none";
+    iframeDashboard.style.display = "none";
+  }
 
   function scrollToSection(target) {
     const section = document.querySelector(target);
